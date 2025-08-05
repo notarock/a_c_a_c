8307e17c3556fbf1d214a7b8fc1a684ffd23f070
Roch D'Amour <roch.damour@gmail.com>
2025-08-05 10:15:26 -0400

    Allow channel owner to trigger acac cmd

--- pkg/runner/messageCountdown.go
+++ pkg/runner/messageCountdown.go
@@ -48,7 +48,7 @@ func NewMessageCountdownRunner(config MessageCountdownConfig) *MessageCountdownR
 			return
 		}
 
-		if runner.client.IsUserModerator(message.User.Name) {
+		if runner.client.IsUserModerator(message.User.Name) || message.User.Name == runner.client.Channel {
 			if message.Message == "!acac" {
 				fmt.Println("Moderator", message.User.Name, "made me speak!", runner.client.Channel)
 				response := runner.chain.GenerateValidMessage(runner.filters) // Generate a valid message
